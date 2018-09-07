f4177c575d1e26da185dd4b44456a2a47a6973d5
Milan Urbanec <milan@urbanec.org>
2018-09-07 15:31:09 +0200

    APP - App.GetOAuthToken GroupV2 - GroupV2.GroupSearch (TODO) - GroupV2.GetGroup - GroupV2.GetMembersOfGroup

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var rp = require('request-promise');
 
 class BungieNetAPI {
     constructor(config = {}) {
-        this.host = 'https:/www.bungie.net';
+        this.host = 'https://www.bungie.net';
         this.key = config.key;
-        this.userAgent = config.userAgent || promiseRequest.globalAgent;
+        this.userAgent = config.userAgent || rp.userAgent;
         this.oauthConfig = {
             id: typeof config.oauthConfig === 'undefined'
                 ? null
@@ -30,17 +30,99 @@ class BungieNetAPI {
     }
 
     /**
-     * oAuth functions
+     * Return Authorization token
+     * @param {string} code
      */
-    getOauthToken(code) {
-        this.options.path = '/Platform/App/OAuth/Token/';
+    app_oAuthToken(code) {
+        this.options = {
+            method: 'POST',
+            uri: this.host + '/Platform/App/OAuth/Token/',
+            form: {
+                client_id: this.oauthConfig.id,
+                grant_type: 'authorization_code',
+                code: code
+            }
+        };
+        return rp(this.options)
+            .then(body => JSON.parse(body))
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
+
+
+    /* GROUP v2 */
+
+    /**
+     * GroupV2.GroupSearch
+     * Search for Groups.
+     * TODO: return error 18 InvalidParameters
+     * @param name
+     */
+    groupV2_Seach(name) {
         this.options.method = 'POST';
+        this.options.uri = this.host + '/Platform/GroupV2/Search/';
         this.options.body = {
-            grant_type: 'authorization_code',
-            client_id: this.oauthConfig.id,
-            code: code
+            // Type: string
+            "name": name,
+            // Type: GroupType:Enum
+            "groupType": 1,
+            // Type: GroupDateRange:Enum
+            "creationDate": 0,
+            // Type: GroupSortBy:Enum
+            "sortBy": 0,
+            // Type: integer:int32:nullable
+            "groupMemberCountFilter": 0,
+            // Type: string
+            "localeFilter": "en",
+            // Type: string
+            //"tagText": "",
+            // Type: integer:int32
+            "itemsPerPage": 10,
+            // Type: integer:int32
+            "currentPage": 0,
+            // Type: string
+            //"requestContinuationToken": {},
+            // Type: GroupQuery
         }
-        return promiseRequest(this.options).then(res => formatJson(res));
+        return rp(this.options)
+            .then(body => body)
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
+
+    /**
+     * GroupV2.GetGroup
+     * Get information about a specific group of the given ID.
+     * @param {int} groupId Requested group's id. Type: int64
+     */
+    groupV2_GetGroup(groupId) {
+        this.options.method = 'GET';
+        this.options.uri = this.host + '/Platform/GroupV2/' + groupId + '/';
+        return rp(this.options)
+            .then(body => body)
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
+
+    /**
+     * GroupV2.GetMembersOfGroup
+     * Get the list of members in a given group.
+     * @param {int} groupId Requested group's id. Type: int64
+     */
+    groupV2_GetMembersOfGroup(groupId) {
+        this.options.method = 'GET';
+        this.options.uri = this.host + '/Platform/GroupV2/' + groupId + '/Members/';
+        return rp(this.options)
+            .then(body => body)
+            .catch(function (err) {
+                console.log(err);
+            });
     }
 
 }
+
+
+module.exports = BungieNetAPI;
